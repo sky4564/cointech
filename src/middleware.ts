39f1f4450ff34336ad09fafda61fdc411bb15401
sky4564 <qw486512@gmail.com>
2024-11-12 20:18:52 +0900

    fix : Delete unused Import

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,6 @@
 import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs';
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
-import { supabase } from '@/lib/supabase';
 
 
 export async function middleware(req: NextRequest) {
